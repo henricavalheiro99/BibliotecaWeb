e8129a99616de53268aaff085aee71372a619d72
livia19gomes <167575522+livia19gomes@users.noreply.github.com>
2024-09-30 17:09:41 -0300

    Fazendo o login

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,9 +1,17 @@
-import css from "./Login.module.css"
+import React, { useState } from 'react';
+import css from './Login.module.css';
+import BlocoCadastroLogin from "../componentes/BlocoCadastroLogin";
 
 export default function Login(){
-    return(
+    return (
         <div>
-
+            <BlocoCadastroLogin
+                frase={"O verdadeiro conhecimento é aquele que capacita as pessoas a se transformarem"}
+                titulo={"Entre na sua conta"}
+                botaoTexto={"Entrar"}
+                textoBaixo={"Não possui conta?"}
+                textoSpan={"Cadastrar"}
+            ></BlocoCadastroLogin>
         </div>
     );
 }
