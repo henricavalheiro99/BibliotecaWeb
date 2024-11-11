9d74340f7f76b5f6ac66e3834c8e3df612eefc25
Henri Cavalheiro <167575522+livia19gomes@users.noreply.github.com>
2024-11-11 07:47:31 -0300

    teste

--- src/paginas/Chat.jsx
+++ src/paginas/Chat.jsx
@@ -1,4 +1,7 @@
 import css from "./Chat.module.css"
+import Header from "../componentes/Header";
+import {FaStar} from "react-icons/fa";
+import React from "react";
 
 export default function Chat(){
     return(
